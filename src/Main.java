8845f5399233e875ea95d680c5700d47625ac34a
jaTomaszu <luzak999@gmail.com>
2019-06-04 19:28:50 +0200

    6 points baby

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.xml.bind.SchemaOutputResolver;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -6,6 +7,8 @@ import java.util.*;
 class Node {
     int number;
     boolean visited = false;
+    boolean processed = false;
+    int time;
     Node p = null;//"parent"
 
     public Node(int number) {
@@ -25,6 +28,7 @@ class Joint {
     Node first;
     Node last;
     int n; //how many routes from first to last
+    String classification = "";
 
     public Joint(Node first, Node last, int n) {
         this.first = first;
@@ -34,7 +38,7 @@ class Joint {
 
     @Override
     public String toString() {
-        return first + "---<" + n + "x>-->" + last;
+        return first + "---<" + n + "x" + classification + ">-->" + last;
     }
 }
 
@@ -45,10 +49,22 @@ class Graf {
 
 
 public class Main {
-
+    static int time = 0;
+    public static String clasify(Joint j){
+            if(j.last.p == j.first)
+                return "TREE";
+            if(j.last.visited && !j.last.processed)
+                return "BACK";
+            if(j.last.processed && j.last.time>j.first.time)
+                return "FORWARD";
+            if(j.last.processed && j.last.time<j.first.time)
+                return "CROSS";
+        return "unclassified";
+    }
     public static void DFS(Graf g) {
         System.out.print("DFS: ");
 
+        time = 0;
         for (Node u : g.nodes) {
             u.visited = false;
             u.p = null;
@@ -57,20 +73,26 @@ public class Main {
             if (!u.visited)
                 DFS(g, u);
         }
+
     }
 
     public static void DFS(Graf g, Node u) {
         u.visited = true;
+        u.time = time;
+        time++;
         Node v;
 
         System.out.print(u + "-->");
 
         for (int i = 0; i < g.joints.size(); i++) {
+            g.joints.get(i).classification = clasify(g.joints.get(i));
             v = g.joints.get(i).last;
             if (g.joints.get(i).first == u && (!v.visited)) {
 
                 v.p = u;
+                v.processed = true;
                 DFS(g, v);
+        time++;
             }
         }
     }
@@ -91,27 +113,30 @@ public class Main {
 
     public static void BFS(Graf g, Node s) {
         Queue<Node> Q = new LinkedList<>();
+        ArrayList<Joint> BFSjoints = new ArrayList<>();
 
         s.visited = true;
         Node v;
 
 
         Q.add(s);
-        System.out.print(s + "-->");
+        System.out.print(s);
 
         while(Q.size()>0) {
             s = Q.poll();
             for (int i = 0; i < g.joints.size(); i++) {
                 v = g.joints.get(i).last;
                 if (g.joints.get(i).first == s && (!v.visited)) {
+                    BFSjoints.add(g.joints.get(i));
 
                     v.visited = true;
                     v.p = s;
                     Q.add(v);
-                    System.out.print(v + "-->");
+                    System.out.print("-->" + v);
                 }
             }
         }
+        System.out.println("\n edges:" + BFSjoints);
     }
 
     public static void main(String[] args) {
@@ -143,9 +168,8 @@ public class Main {
             }
         }
 
-        System.out.println(graf.joints);
-
         DFS(graf);
+        System.out.println("\n" + graf.joints);
         System.out.println();
         BFS(graf);
     }
